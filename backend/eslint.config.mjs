fe7fa1796c737d456db123622e495b81f50c5849
ashley-koh <ashleykoh24@gmail.com>
2024-07-04 00:01:52 +0800

    feat: added prettier to backend proj

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -1,10 +1,15 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default [
-  { files: ["**/*.{ts}"] },
+  { files: ['src/**/*.{js,cjs,mjs,ts}'] },
+  {
+    ignores: ['dist/**/*.{js,cjs,mjs,ts}', 'node_modules/**/*.{js,cjs,mjs,ts}'],
+  },
   { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  eslintConfigPrettier,
 ];
