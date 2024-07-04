f298f417c4bd811ef5247338689410f860391843
ashley-koh <ashleykoh24@gmail.com>
2024-07-04 11:43:10 +0800

    backend - fix: fixed eslint and prettier setup

---
+++ backend/.eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "import"],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      extends: [
+        "airbnb-base",
+        "airbnb-typescript/base",
+        "prettier",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+      ],
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        project: "tsconfig.json",
+        sourceType: "module",
+      },
+    },
+  ],
+};
