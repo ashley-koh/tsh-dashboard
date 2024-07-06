cf7f2428814e605493cd73a4211cccfa91fc46bf
ashley-koh <ashleykoh24@gmail.com>
2024-07-06 23:12:51 +0800

    backend - fix: added auth to database connection

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -3,4 +3,14 @@ import { config } from 'dotenv';
 config({ path: `.env.${process.env.NODE_ENV || 'development'}.local` });
 
 export const CREDENTIALS = process.env.CREDENTIALS === 'true';
-export const { NODE_ENV, PORT, DB_HOST, DB_PORT, DB_DATABASE, DB_USERNAME, DB_PASSWORD, SECRET_KEY, ORIGIN } = process.env;
+export const {
+  NODE_ENV,
+  PORT,
+  DB_HOST,
+  DB_PORT,
+  DB_DATABASE,
+  DB_USERNAME,
+  DB_PASSWORD,
+  SECRET_KEY,
+  ORIGIN,
+} = process.env;
--- backend/src/databases/index.ts
+++ backend/src/databases/index.ts
@@ -1,4 +1,10 @@
-import { DB_HOST, DB_PORT, DB_DATABASE, DB_USERNAME, DB_PASSWORD } from '@config';
+import {
+  DB_HOST,
+  DB_PORT,
+  DB_DATABASE,
+  DB_USERNAME,
+  DB_PASSWORD,
+} from '@config';
 
 const dbConnection = {
   url: `mongodb://${DB_USERNAME}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}?authSource=admin`,
