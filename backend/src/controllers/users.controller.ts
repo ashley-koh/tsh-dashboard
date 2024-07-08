dd9024494f626923feab26d7ff6db247c6454238
XavierTann <164714627+XavierTann@users.noreply.github.com>
2024-07-08 15:03:15 +0800

    backend - feat: added additional attributes to user model

--- backend/src/controllers/users.controller.ts
+++ backend/src/controllers/users.controller.ts
@@ -1,10 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
 import { CreateUserDto } from '@dtos/users.dto';
 import { User } from '@interfaces/users.interface';
-import userService from '@services/users.service';
+import UserService from '@services/users.service';
 
 class UsersController {
-  public userService = new userService();
+  public userService = new UserService();
 
   public getUsers = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -16,7 +16,11 @@ class UsersController {
     }
   };
 
-  public getUserById = async (req: Request, res: Response, next: NextFunction) => {
+  public getUserById = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
     try {
       const userId: string = req.params.id;
       const findOneUserData: User = await this.userService.findUserById(userId);
@@ -27,7 +31,11 @@ class UsersController {
     }
   };
 
-  public createUser = async (req: Request, res: Response, next: NextFunction) => {
+  public createUser = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
     try {
       const userData: CreateUserDto = req.body;
       const createUserData: User = await this.userService.createUser(userData);
@@ -38,11 +46,18 @@ class UsersController {
     }
   };
 
-  public updateUser = async (req: Request, res: Response, next: NextFunction) => {
+  public updateUser = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
     try {
       const userId: string = req.params.id;
       const userData: CreateUserDto = req.body;
-      const updateUserData: User = await this.userService.updateUser(userId, userData);
+      const updateUserData: User = await this.userService.updateUser(
+        userId,
+        userData,
+      );
 
       res.status(200).json({ data: updateUserData, message: 'updated' });
     } catch (error) {
@@ -50,7 +65,11 @@ class UsersController {
     }
   };
 
-  public deleteUser = async (req: Request, res: Response, next: NextFunction) => {
+  public deleteUser = async (
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) => {
     try {
       const userId: string = req.params.id;
       const deleteUserData: User = await this.userService.deleteUser(userId);
--- backend/src/dtos/users.dto.ts
+++ backend/src/dtos/users.dto.ts
@@ -1,9 +1,39 @@
-import { IsEmail, IsString } from "class-validator";
+import {
+  IsDate,
+  IsEmail,
+  IsIn,
+  IsString,
+  IsStrongPassword,
+} from 'class-validator';
 
 export class CreateUserDto {
   @IsEmail()
   public email: string;
 
-  @IsString()
+  @IsStrongPassword({
+    minLength: 10,
+    minLowercase: 1,
+    minUppercase: 1,
+    minNumbers: 1,
+    minSymbols: 1,
+  })
   public password: string;
+
+  @IsString()
+  public firstName: string;
+
+  @IsString()
+  public lastName: string;
+
+  @IsIn(['employee', 'head_of_department', 'business_owner'])
+  public role: string;
+
+  @IsIn(['hr', 'fa', 'mk'])
+  public dept: string;
+
+  @IsDate()
+  public date: Date;
+
+  @IsIn(['full_time', 'part_time', 'intern', 'temp'])
+  public employeeStatus: string;
 }
