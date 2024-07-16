2e0b05cccd6db55d7f58286c63384775702ac946
XavierTann <164714627+XavierTann@users.noreply.github.com>
2024-07-16 15:29:38 +0800

    backend - feat: added CRUD routes for form

---
+++ backend/src/routes/form.route.ts
@@ -0,0 +1,39 @@
+import { Router } from 'express';
+import FormController from '@controllers/form.controller';
+import { Routes } from '@interfaces/routes.interface';
+import { CreateFormDto } from '@dtos/form.dto';
+import validationMiddleware from '@middlewares/validation.middleware';
+
+class FormRoutes implements Routes {
+  public path = '/form';
+
+  public router = Router();
+
+  public formController = new FormController();
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  private initializeRoutes() {
+    this.router.post(
+      `${this.path}/createForm`,
+      validationMiddleware(CreateFormDto, 'body'),
+      this.formController.createQuestion,
+    );
+
+    this.router.get(`${this.path}/:id`, this.formController.getFormById);
+
+    this.router.get(`${this.path}`, this.formController.getForms);
+
+    this.router.put(
+      `${this.path}/:id`,
+      validationMiddleware(CreateFormDto, 'body', true),
+      this.formController.updateForm,
+    );
+
+    this.router.delete(`${this.path}/:id`, this.formController.deleteForm);
+  }
+}
+
+export default FormRoutes;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import IndexRoute from '@routes/index.route';
 import UsersRoute from '@routes/users.route';
 import validateEnv from '@utils/validateEnv';
 import QuestionRoute from '@routes/question.route';
+import FormRoute from '@routes/form.route';
 import App from './app';
 
 validateEnv();
@@ -12,6 +13,7 @@ const app = new App([
   new UsersRoute(),
   new AuthRoute(),
   new QuestionRoute(),
+  new FormRoute(),
 ]);
 
 app.listen();
