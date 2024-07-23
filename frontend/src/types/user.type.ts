1a24b4248aef5ac65f23be04cb086d2ae84a602d
ashley-koh <ashleykoh24@gmail.com>
2024-07-23 20:04:35 +0800

    frontend - feat: added user details to navbar

--- frontend/src/types/user.type.ts
+++ frontend/src/types/user.type.ts
@@ -1,27 +1,12 @@
-enum Role {
-  Employee = "employee",
-  HeadOfDepartment = "head_of_department",
-  BusinessOwner = "business_owner",
-  HR = "hr"
-}
-
-enum EmploymentStatus {
-  FullTime = "full_time",
-  PartTime = "part_time",
-  Intern = "intern",
-  Temp = "temp",
-}
-
 type User = {
   name: string;
   email: string;
   password: string;
   employeeID: string;
-  role: Role;
+  role: "employee" | "head_of_department" | "business_owner";
   jobTitle: string;
   dept: string;
-  employmentStatus: EmploymentStatus;
+  employmentStatus: "full_time" | "part_time" | "intern" | "temp";
 };
 
 export default User;
-export { Role, EmploymentStatus };
