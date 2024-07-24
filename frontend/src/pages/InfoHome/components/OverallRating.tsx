b8d07f4bfb81ca353fa975401050d6f663c6a596
Zayne Siew <81795965+zayne-siew@users.noreply.github.com>
2024-07-25 00:45:11 +0800

    feat: custom styling for circular progress bar

--- frontend/src/pages/InfoHome/components/OverallRating.tsx
+++ frontend/src/pages/InfoHome/components/OverallRating.tsx
@@ -1,14 +1,21 @@
 import React from "react";
-import { Card, Flex, Progress } from "antd";
+import { Card, Flex, Progress, ProgressProps } from "antd";
+import './OverallRating.css';
 
 const OverallRating: React.FC<{
   rating: number;
 }> = ({ rating }) => {
+
+  const twoColors: ProgressProps["strokeColor"] = {
+    "0%": "#eb2a27",
+    "100%": "#108ee9",
+  };
+
   return (
     <>
       <Card title="Overall Rating">
         <Flex justify="center">
-          <Progress type="dashboard" percent={rating} size={256} />
+          <Progress type="dashboard" percent={rating} size={256} strokeColor={twoColors} gapDegree={60} />
         </Flex>
       </Card>
     </>
